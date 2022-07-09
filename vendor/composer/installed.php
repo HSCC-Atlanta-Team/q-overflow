a723125aab42d0e67ff7be83c07e584f27e2e32a
Suchit Vemula <vemulasuchit3@gmail.com>
2022-07-09 14:29:26 +0000

    initial libraries

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'hscc/qoverflow',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hscc/qoverflow' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
