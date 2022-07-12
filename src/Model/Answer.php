166af1c1c4bf1b93a74ce3db309babc80e3bf528
Suchit Vemula <vemulasuchit3@gmail.com>
2022-07-12 19:38:42 +0000

    added Answer Model

---
+++ src/Model/Answer.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Qoverflow\Model;
+
+class Answer extends Model
+{
+    protected $answer_id;
+    protected $creator;
+    protected $createdAt;
+    protected $text;
+    protected $upvotes;
+    protected $downvotes;
+    protected $accepted;
+
+
+
+    /**
+     * Get the value of answer_id
+     */
+    public function getAnswerId()
+    {
+        return $this->answer_id;
+    }
+
+    /**
+     * Set the value of answer_id
+     */
+    public function setAnswerId($answer_id): self
+    {
+        $this->answer_id = $answer_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of creator
+     */
+    public function getCreator()
+    {
+        return $this->creator;
+    }
+
+    /**
+     * Set the value of creator
+     */
+    public function setCreator($creator): self
+    {
+        $this->creator = $creator;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of createdAt
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set the value of createdAt
+     */
+    public function setCreatedAt($createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of text
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set the value of text
+     */
+    public function setText($text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of upvotes
+     */
+    public function getUpvotes()
+    {
+        return $this->upvotes;
+    }
+
+    /**
+     * Set the value of upvotes
+     */
+    public function setUpvotes($upvotes): self
+    {
+        $this->upvotes = $upvotes;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of downvotes
+     */
+    public function getDownvotes()
+    {
+        return $this->downvotes;
+    }
+
+    /**
+     * Set the value of downvotes
+     */
+    public function setDownvotes($downvotes): self
+    {
+        $this->downvotes = $downvotes;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of accepted
+     */
+    public function getAccepted()
+    {
+        return $this->accepted;
+    }
+
+    /**
+     * Set the value of accepted
+     */
+    public function setAccepted($accepted): self
+    {
+        $this->accepted = $accepted;
+
+        return $this;
+    }
+}
