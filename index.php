17dc6517c0aa1e6d69748a173de391c5387e61fc
Sowrya Chiluvuri <sowchils2006@gmail.com>
2022-07-29 00:48:24 +0000

    removed debug

--- index.php
+++ index.php
@@ -9,21 +9,7 @@ session_start();
 
 
 $f3 = \Base::instance();
-$f3->route('GET /', function($f3){
 
-
-
-    $repo = new UserRepository($f3);
-    $userModel = new User([
-        'username' => 'RQdoXQn87S',
-        'points' => 12,
-    ]);
-
-    $user = $repo->changePassword('ouhnoujh', 'candy');
-
-    dd($user);
-    $question = $repo->getUserQuestions();
-});
 $f3->config('config/globals.cfg');
 $f3->config('config/routes.cfg');
 $f3->config('config/secrets.cfg');
