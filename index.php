88d02ecbbdf26921a7dc6d538cd428f9cc90f90a
Sowrya Chiluvuri <sowchils2006@gmail.com>
2022-08-06 15:16:43 +0000

    questions template and test

--- index.php
+++ index.php
@@ -8,7 +8,19 @@ use Qoverflow\Controller\LoginController;
 session_start();
 
 $f3 = \Base::instance();
+$f3->route('GET /test', function ($f3) {
+    if ($f3->get('ENVIRONMENT') == 'production') {
+        $f3->reroute('GET /');
+    }
 
+    $template = sprintf(
+        'templates/%s.html',
+        $f3->get('REQUEST.template')
+    );
+    
+    $f3->set('template', $template);
+    echo \Template::instance()->render('templates/main.html');
+});
 $f3->config('config/globals.cfg');
 $f3->config('config/routes.cfg');
 $f3->config('config/secrets.cfg');
