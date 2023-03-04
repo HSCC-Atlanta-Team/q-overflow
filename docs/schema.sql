f418ad13f0259bbafce6212fd136ebd44085de42
Jamie Kohns <jamiekohns@gmail.com>
2023-03-04 11:38:25 -0500

    Added question table

--- docs/schema.sql
+++ docs/schema.sql
@@ -9,3 +9,20 @@ CREATE TABLE `user` (
 	PRIMARY KEY (`username`)
 ) ENGINE=InnoDB;
 
+DROP TABLE IF EXISTS `question`;
+
+CREATE TABLE `question` (
+	`question_id` VARCHAR(64) NOT NULL,
+	`creator` VARCHAR(64) NOT NULL,
+	`createdAt` INT(11) NOT NULL,
+	`status` VARCHAR(32),
+	`title` VARCHAR(255),
+	`text` TEXT,
+	`views` INT(11) DEFAULT 0,
+	`answers` INT(11) DEFAULT 0,
+	`comments` INT(11) DEFAULT 0,
+	`upvotes` INT(11) DEFAULT 0,
+	`downvotes` INT(11) DEFAULT 0,
+	`hasAcceptedAnswer` TINYINT(1) DEFAULT 0,
+	PRIMARY KEY (`question_id`)
+) ENGINE=InnoDB;
