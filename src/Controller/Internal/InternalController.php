9f24aa25a6a6581ffbaab35cf5b0a2aca9400b9d
Jamie Kohns <jamiekohns@gmail.com>
2023-04-08 11:58:50 -0400

    Add comments

--- src/Controller/Internal/InternalController.php
+++ src/Controller/Internal/InternalController.php
@@ -6,15 +6,21 @@ class InternalController
 {
     public function beforeroute($f3)
     {
-        // check for logged in user
+        /* we should check for logged in user here
+        also, maybe check that this is not being loaded 
+        by iteself, by checking the `referrer` */
     }
 
+    /**
+     * afterroute will be executed after the main controller method has completed
+     */
     public function afterroute($f3)
     {
+        // IF `json` is set, then we know that the intention was to return JSON data
         if (isset($f3->json)) {
             header('Content-Type: application/json');
             echo json_encode($f3->json);
-        } else {
+        } else { // otherwise, render the template as a snippet
             echo \Template::instance()->render($f3->template);
         }
     }
--- src/Controller/Internal/UsersController.php
+++ src/Controller/Internal/UsersController.php
@@ -6,6 +6,7 @@ class UsersController extends InternalController
 {
     public function myself($f3)
     {
+        // Data mock: this is just sample data we set up (temporarily) to test our code
         $currentUser = [
             "username" => "Willie0",
             "email" => "willie0@whs.edu",
@@ -16,12 +17,16 @@ class UsersController extends InternalController
             "questions" => 0,
         ];
 
+        // set the variable in out application (referenced as $f3)
         $f3->set('currentUser', $currentUser);
+        // set a `template` variable, so our afterroute method of the base class will know
+        // what to render
         $f3->set('template', 'templates/component/current_user.html');
     }
 
     public function getUsers($f3)
     {
+        // Data mock
         $users = [
             [
                 'username' => 'test1',
@@ -34,6 +39,7 @@ class UsersController extends InternalController
             ],
         ];
 
+        // here, we set the `json` variable, so afterroute will know to return as JSON
         $f3->set('json', $users);
     }
 }
